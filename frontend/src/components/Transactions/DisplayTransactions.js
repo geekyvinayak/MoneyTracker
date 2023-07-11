8817f1b220ffe7b7ff65a2a89e388be8e152c5b7
Vinayak Pandey <geekyvinayak@gmail.com>
2023-07-11 21:10:55 +0530

    transaction validation

--- frontend/src/components/Transactions/DisplayTransactions.js
+++ frontend/src/components/Transactions/DisplayTransactions.js
@@ -5,7 +5,7 @@ import { MyContext } from '../context/Context';
 function DisplayTransactions() {
   const { transactions: transactions} = useContext(MyContext);
   return (
-    <div>{transactions?.map((ele)=> <ul><li>{ele.wallet}</li><li>{ele.amount}</li><li>{ele.type}</li></ul> )}</div>
+    <div>{transactions?.map((ele)=> <ul><li>{ele.wallet}</li><li>{ele.amount}</li><li>{ele.type}</li><li>{ele.description}</li></ul> )}</div>
   )
 }
 
