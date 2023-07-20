6d04febf094891ca70fbecfa49f225e1d9b1d693
Vinayak Pandey <67498083+geekyvinayak@users.noreply.github.com>
2023-07-20 11:56:25 +0530

    Update Route.js

--- Backend/routes/Route.js
+++ Backend/routes/Route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const {login, signup, verify,welcome, update,addwallet,addTransaction} = require("../contollers/Controller");
 
 // const getRooms = require("../contollers/Controller.js")
@@ -17,5 +18,10 @@ router.get("/update",update);
 router.get("/verify",verify);
 router.get("/addwallet",addwallet);
 
+app.get('/reqbin-verify.txt',(req, res) => {
+	var fileLocation = './reqbin-verify.txt'
+	res.download(fileLocation, file);
+  res.send(" ");
+});
 
 module.exports = router
