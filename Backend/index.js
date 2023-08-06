3592e2950492b4e8a995123f579e08bd0f05c263
Vinayak Pandey <67498083+geekyvinayak@users.noreply.github.com>
2023-08-06 15:55:32 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,6 +16,14 @@ var corsOptions = {
     optionsSuccessStatus: 200 // For legacy browser support
 }
 
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(cors(corsOptions));
 
